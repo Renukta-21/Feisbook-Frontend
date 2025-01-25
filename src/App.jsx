d992120b169273e1b1291b7d88656dcaffbbca02
Daniel Martinez <edu211004@gmail.com>
2025-01-25 14:39:27 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NewUserForm from "./components/NewUserForm"
+
+function App() {
+  return (
+    <div>
+      <h2>Feisbook</h2>
+      <NewUserForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NewUserForm.jsx
@@ -0,0 +1,11 @@
+function NewUserForm() {
+  return (
+    <div>
+        <form action="">
+            <label htmlFor=""></label><input type="text" />
+        </form>
+    </div>
+  )
+}
+
+export default NewUserForm
