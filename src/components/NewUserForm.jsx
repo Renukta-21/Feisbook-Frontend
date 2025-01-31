92a9bf806831be7bb2a1b30f559fdf432e739c09
Daniel Martinez <edu211004@gmail.com>
2025-01-31 11:08:38 -0600

    handling fields

--- src/components/NewUserForm.jsx
+++ src/components/NewUserForm.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react"
+
 function NewUserForm() {
+  const [fields, setFields] = useState({
+    email:'',
+    password:'',
+    name:'',
+    surname:''
+  })
+
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+    console.log(fields)
+  }
+
+  const updateField = (e,fieldName)=>{
+    setFields(prevFields=>({
+      ...prevFields,
+      [fieldName]:e.target.value
+    }))
+    console.log(fields)
+  }
   return (
     <div>
-        <form action="">
-            <label htmlFor=""></label><input type="text" />
+        <form action="" onSubmit={handleSubmit}>
+            <label htmlFor="">Email</label><input type="text" onChange={(e)=> updateField(e,'email')}/>
+            <label htmlFor="">New password</label><input type="text" onChange={(e)=> updateField(e,'password')}/>
+            <label htmlFor="">Name</label><input type="text" onChange={(e)=> updateField(e,'name')}/>
+            <label htmlFor="">Surname</label><input type="text" onChange={(e)=> updateField(e,'surname')}/>
+            <button type="submit">Register</button>
         </form>
     </div>
   )
